2d4ef98fd9eddfd3b4b93a1998f6231f18d5b6a5
Shunichi Yamamoto <yamamo2shun1@gmail.com>
2025-06-18 13:57:26 +0900

    Modified VID and PID.

--- STM32CubeIDE/USB_HS_TEST/Appli/USB_DEVICE/App/usbd_desc.c
+++ STM32CubeIDE/USB_HS_TEST/Appli/USB_DEVICE/App/usbd_desc.c
@@ -62,11 +62,11 @@
   * @{
   */
 
-#define USBD_VID     1155
+#define USBD_VID     0x31BF
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID     22336
-#define USBD_PRODUCT_STRING     "STM32 Virtual ComPort"
+#define USBD_MANUFACTURER_STRING     "Yamamoto Works Ltd."
+#define USBD_PID     0x0010
+#define USBD_PRODUCT_STRING     "C4NDY::02"
 #define USBD_CONFIGURATION_STRING     "CDC Config"
 #define USBD_INTERFACE_STRING     "CDC Interface"
 
