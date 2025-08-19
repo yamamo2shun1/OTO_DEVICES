0b4daed95acc8ed144f849f04bce756668873eb5
Shunichi Yamamoto <yamamo2shun1@gmail.com>
2025-08-19 14:34:14 +0900

    Added exported system files.

---
+++ SigmaDSP/exported_code/oto_no_ita_dsp_ADAU146xSchematic_1_Defines.h
@@ -0,0 +1,24 @@
+/* 
+ * File:	C:\Users\shun\OTO_DEVICES\SigmaDSP\exported_code\oto_no_ita_dsp_ADAU146xSchematic_1_Defines.h
+ * Created:	Tuesday, 19 August 2025 14:27 PM
+ * Description:	Schematic default download data definitions.
+ * 
+ * This software is distributed in the hope that it will be useful, 
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * 
+ * This software may only be used to program products purchased from 
+ * Analog Devices for incorporation by you into audio products that 
+ * are intended for resale to audio product end users.This software 
+ * may not be distributed whole or in any part to third parties. 
+ * 
+ * Copyright © 2025 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __OTO_NO_ITA_DSP_ADAU146XSCHEMATIC_1_DEFINES_H__
+#define __OTO_NO_ITA_DSP_ADAU146XSCHEMATIC_1_DEFINES_H__
+
+#define BufferSize_ADAU146XSCHEMATIC_1			734
+#define NumTransactions_ADAU146XSCHEMATIC_1		58
+
+#endif
