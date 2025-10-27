710150024ea544363ad3b88ed5f6d04f2cd750de
Shunichi Yamamoto <yamamo2shun1@gmail.com>
2025-10-27 21:39:32 +0900

    SAIとPWMで使うGPIOの速度設定を変更

--- STM32CubeIDE/USB_HS_TEST/Appli/Core/Src/sai.c
+++ STM32CubeIDE/USB_HS_TEST/Appli/Core/Src/sai.c
@@ -166,7 +166,7 @@ void HAL_SAI_MspInit(SAI_HandleTypeDef* saiHandle)
         GPIO_InitStruct.Pin       = GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6;
         GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
         GPIO_InitStruct.Pull      = GPIO_NOPULL;
-        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
         GPIO_InitStruct.Alternate = GPIO_AF6_SAI1;
         HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
@@ -262,14 +262,14 @@ void HAL_SAI_MspInit(SAI_HandleTypeDef* saiHandle)
         GPIO_InitStruct.Pin       = GPIO_PIN_11 | GPIO_PIN_12;
         GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
         GPIO_InitStruct.Pull      = GPIO_NOPULL;
-        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
         GPIO_InitStruct.Alternate = GPIO_AF10_SAI2;
         HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
         GPIO_InitStruct.Pin       = GPIO_PIN_13;
         GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
         GPIO_InitStruct.Pull      = GPIO_NOPULL;
-        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
         GPIO_InitStruct.Alternate = GPIO_AF8_SAI2;
         HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
