15bb0cd624c332776fe3e6e640fc4e084c2e2a22
Shunichi Yamamoto <yamamo2shun1@gmail.com>
2025-11-09 09:02:03 +0900

    USB_HS_TESTのAppliを新規プロジェクトとして作り直す

---
+++ STM32CubeIDE/JUMBLEQ/Appli/Core/Src/SigmaStudioFW.c
@@ -0,0 +1,51 @@
+/*
+ * SigmaStudioFW.c
+ *
+ *  Created on: Aug 8, 2025
+ *      Author: shun
+ */
+
+#include "SigmaStudioFW.h"
+
+#include "spi.h"
+
+void SIGMA_WRITE_REGISTER_BLOCK(uint8_t devAddress, uint16_t address, uint16_t length, uint8_t* pData)
+{
+    // HAL_StatusTypeDef status;
+    uint8_t data[1 + 2 + length];
+
+    data[0] = devAddress;
+    data[1] = (uint8_t) ((address >> 8) & 0x00FF);
+    data[2] = (uint8_t) (address & 0x00FF);
+    for (int i = 0; i < length; i++)
+    {
+        data[i + 3] = pData[i];
+    }
+    HAL_StatusTypeDef status = HAL_SPI_Transmit(&hspi5, data, 1 + 2 + length, 100);
+#if 1
+    if (status != HAL_OK)
+    {
+        printf("[%X] spi write error\n", address);
+    }
+#endif
+}
+
+void SIGMA_SAFELOAD_WRITE_DATA(uint8_t devAddress, uint16_t dataAddress, uint16_t length, uint8_t* pData)
+{
+    // HAL_StatusTypeDef status;
+    uint8_t data[1 + 2 + length];
+
+    data[0] = devAddress;
+    data[1] = (uint8_t) ((dataAddress >> 8) & 0x00FF);
+    data[2] = (uint8_t) (dataAddress & 0x00FF);
+    for (int i = 0; i < length; i++)
+    {
+        data[i + 3] = pData[i];
+    }
+    HAL_SPI_Transmit(&hspi5, data, 1 + 2 + length, 100);
+}
+
+void SIGMA_WRITE_DELAY(uint8_t devAddress, uint16_t dataAddress, uint16_t length, uint8_t* pData)
+{
+    HAL_Delay(15);
+}
