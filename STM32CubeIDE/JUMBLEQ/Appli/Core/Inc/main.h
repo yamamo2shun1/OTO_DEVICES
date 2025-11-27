3e36d9e8f08bedb247a2dd0cf0f91a6de00af9a4
Shunichi Yamamoto <yamamo2shun1@gmail.com>
2025-11-27 14:30:04 +0900

    tinyusbのデバッグ・ログをRTTで出力出来るようにするためのコード追加

--- STM32CubeIDE/JUMBLEQ/Appli/Core/Inc/main.h
+++ STM32CubeIDE/JUMBLEQ/Appli/Core/Inc/main.h
@@ -30,24 +30,15 @@ extern "C"
 /* Includes ------------------------------------------------------------------*/
 #include "stm32h7rsxx_hal.h"
 
-#include "stm32h7rsxx_ll_ucpd.h"
-#include "stm32h7rsxx_ll_bus.h"
-#include "stm32h7rsxx_ll_cortex.h"
-#include "stm32h7rsxx_ll_rcc.h"
-#include "stm32h7rsxx_ll_system.h"
-#include "stm32h7rsxx_ll_utils.h"
-#include "stm32h7rsxx_ll_pwr.h"
-#include "stm32h7rsxx_ll_gpio.h"
-#include "stm32h7rsxx_ll_dma.h"
-
-#include "stm32h7rsxx_ll_exti.h"
-
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "stdio.h"
 
 #include "tusb.h"
 #include "usb_descriptors.h"
+
+#include "SEGGER_RTT.h"
+#include "my_log.h"
     /* USER CODE END Includes */
 
     /* Exported types ------------------------------------------------------------*/
