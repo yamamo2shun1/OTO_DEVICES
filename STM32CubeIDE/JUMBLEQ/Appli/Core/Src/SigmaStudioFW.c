9fa4fa47ba31da02d57e63b88e9cb6996ba0e42d
Shunichi Yamamoto <yamamo2shun1@gmail.com>
2025-12-12 15:01:11 +0900

    SPI送信時、前の転送が終わっているかチェックする＆エラー処理入れる

--- STM32CubeIDE/JUMBLEQ/Appli/Core/Src/SigmaStudioFW.c
+++ STM32CubeIDE/JUMBLEQ/Appli/Core/Src/SigmaStudioFW.c
@@ -9,9 +9,18 @@
 
 #include "spi.h"
 
-bool spi_tx_complete = true;
+static volatile bool spi_tx_complete = true;
+static uint8_t spi_tx_buf[16];
 
 void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef* hspi)
+{
+    if (hspi == &hspi5)
+    {
+        spi_tx_complete = true;
+    }
+}
+
+void HAL_SPI_ErrorCallback(SPI_HandleTypeDef* hspi)
 {
     if (hspi == &hspi5)
     {
@@ -41,26 +50,25 @@ void SIGMA_WRITE_REGISTER_BLOCK(uint8_t devAddress, uint16_t address, uint16_t l
 
 void SIGMA_WRITE_REGISTER_BLOCK_IT(uint8_t devAddress, uint16_t address, uint16_t length, uint8_t* pData)
 {
-    // HAL_StatusTypeDef status;
-    uint8_t data[1 + 2 + length];
-
-    data[0] = devAddress;
-    data[1] = (uint8_t) ((address >> 8) & 0x00FF);
-    data[2] = (uint8_t) (address & 0x00FF);
-    for (int i = 0; i < length; i++)
-    {
-        data[i + 3] = pData[i];
-    }
-
     while (!spi_tx_complete)
     {
         // wait previous transmission complete
+        __NOP();
     }
 
-    HAL_StatusTypeDef status = HAL_SPI_Transmit_IT(&hspi5, data, 1 + 2 + length);
+    spi_tx_buf[0] = devAddress;
+    spi_tx_buf[1] = (uint8_t) ((address >> 8) & 0x00FF);
+    spi_tx_buf[2] = (uint8_t) (address & 0x00FF);
+    for (int i = 0; i < length; i++)
+    {
+        spi_tx_buf[i + 3] = pData[i];
+    }
 
+    spi_tx_complete          = false;
+    HAL_StatusTypeDef status = HAL_SPI_Transmit_IT(&hspi5, spi_tx_buf, 1 + 2 + length);
     if (status != HAL_OK)
     {
+        spi_tx_complete = true;
         printf("[%X] spi write error\n", address);
     }
 }
