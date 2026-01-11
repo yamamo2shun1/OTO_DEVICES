773d6d2658e1ec3d8f11213f0cd3df1cf1d90cdf
Shunichi Yamamoto <yamamo2shun1@gmail.com>
2026-01-11 09:26:44 +0900

    ReadBackのブロック名変更に対応

--- STM32CubeIDE/JUMBLEQ/Appli/Core/Src/led_control.c
+++ STM32CubeIDE/JUMBLEQ/Appli/Core/Src/led_control.c
@@ -112,7 +112,7 @@ void renew(void)
 void rgb_led_task(void)
 {
     ADI_REG_TYPE rx_data[4] = {0};
-    SIGMA_READ_REGISTER(DEVICE_ADDR_ADAU146XSCHEMATIC_1, MOD_DSPREADBACK_0_VALUE_ADDR, 4, rx_data);
+    SIGMA_READ_REGISTER(DEVICE_ADDR_ADAU146XSCHEMATIC_1, MOD_DSPREADBACK_B_VALUE_ADDR, 4, rx_data);
     uint32_t val = rx_data[0] << 24 | rx_data[1] << 16 | rx_data[2] << 8 | rx_data[3];
     float dbfs   = 0.0f;
     if (val == 0)
