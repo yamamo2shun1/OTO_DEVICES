00e82b56dfb0767bc36a9ab787a2349c221b642b
Shunichi Yamamoto <yamamo2shun1@gmail.com>
2026-03-06 08:50:34 +0900

    【SigmaStudio+】USBから戻ってくる経路にVolumeブロックを追加

--- SigmaDSP/exported_code/JUMBLEQ_DSP_ADAU146xSchematic_1_Defines.h
+++ SigmaDSP/exported_code/JUMBLEQ_DSP_ADAU146xSchematic_1_Defines.h
@@ -1,6 +1,6 @@
 /* 
  * File:	C:\Users\shuni\JUMBLEQ\SigmaDSP\exported_code\JUMBLEQ_DSP_ADAU146xSchematic_1_Defines.h
- * Created:	Thursday, 05 March 2026 13:38 PM
+ * Created:	Friday, 06 March 2026 8:47 AM
  * Description:	Schematic default download data definitions.
  * 
  * This software is distributed in the hope that it will be useful, 
@@ -18,7 +18,7 @@
 #ifndef __JUMBLEQ_DSP_ADAU146XSCHEMATIC_1_DEFINES_H__
 #define __JUMBLEQ_DSP_ADAU146XSCHEMATIC_1_DEFINES_H__
 
-#define BufferSize_ADAU146XSCHEMATIC_1			3790
+#define BufferSize_ADAU146XSCHEMATIC_1			3806
 #define NumTransactions_ADAU146XSCHEMATIC_1		58
 
 #endif
