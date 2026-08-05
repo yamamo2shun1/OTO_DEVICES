89e8cb4428fd8b3b950c2bd5a3dd04558e085352
Shunichi Yamamoto <yamamo2shun1@gmail.com>
2026-08-05 15:19:57 +0900

    refactor(dsp): 出力段のSplitterを削除
    SigmaStudio+の直接分岐に置き換え、DSPプログラムと生成ヘッダーを更新

--- SigmaDSP/exported_code/JUMBLEQ_DSP_ADAU146xSchematic_1_Defines.h
+++ SigmaDSP/exported_code/JUMBLEQ_DSP_ADAU146xSchematic_1_Defines.h
@@ -1,6 +1,6 @@
 /* 
  * File:	C:\Users\shun\JUMBLEQ\SigmaDSP\exported_code\JUMBLEQ_DSP_ADAU146xSchematic_1_Defines.h
- * Created:	Wednesday, 05 August 2026 14:22 PM
+ * Created:	Wednesday, 05 August 2026 15:13 PM
  * Description:	Schematic default download data definitions.
  * 
  * This software is distributed in the hope that it will be useful, 
@@ -18,7 +18,7 @@
 #ifndef __JUMBLEQ_DSP_ADAU146XSCHEMATIC_1_DEFINES_H__
 #define __JUMBLEQ_DSP_ADAU146XSCHEMATIC_1_DEFINES_H__
 
-#define BufferSize_ADAU146XSCHEMATIC_1			4742
+#define BufferSize_ADAU146XSCHEMATIC_1			4638
 #define NumTransactions_ADAU146XSCHEMATIC_1		70
 
 #endif
